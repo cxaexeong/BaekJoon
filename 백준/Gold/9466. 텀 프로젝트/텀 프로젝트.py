e7e539c9b1227e5edbeb857947881ff29e8f0850
ChaeYeong Lee <130729886+cxaexeong@users.noreply.github.com>
2024-10-28 13:54:19 +0900

    [Gold III] Title: 텀 프로젝트, Time: 3180 ms, Memory: 61676 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,26 +1,26 @@
 import sys
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 def dfs(start, arr):
-    visited[start] = 1
-    cycle.append(start)
+    v[start] = 1
+    team.append(start)
     num = nums[start]
-    if visited[num]:
-        if num in cycle:
-            arr += cycle[cycle.index(num):]
+    if v[num]:
+        if num in team:
+            arr += team[team.index(num):]
         return
     else:
         dfs(num, arr)
-
+        
 for _ in range(int(input())):
     n = int(input())
     nums = [0] + list(map(int, input().split()))
-    visited = [0 for _ in range(n + 1)]
+    v = [0 for _ in range(n+1)]
     # 팀을 이룬 학생
     student = []
-    for now in range(1, n + 1):
-        if not visited[now]:
-            cycle = []
-            dfs(now, student)
+    for i in range(1,n+1):
+        if not v[i]:
+            team = []
+            dfs(i, student)
     print(n - len(student))
